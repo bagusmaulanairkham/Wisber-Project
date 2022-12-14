83857518aeaaf14260c052a5b286231d10724742
Bilas Febri Dwi Ananda <bilasbwi@gmail.com>
2022-12-14 12:34:05 +0700

    prosesinputuser

--- prosesinputuser.php
+++ prosesinputuser.php
@@ -8,20 +8,20 @@ $password = $_POST['password'];
 $Konfirmasi=$_POST['KonfirmasiPassword'];
 
 $query = "INSERT INTO user (nama_desa, email, password) 
-        VALUE ('$nama_desa', '$email', '$password') ";
-
-$hasil = mysqli_query ($conn, $query);
+VALUE ('$nama_desa', '$email', '$password')";
 
 if ($password != $Konfirmasi) {  
     echo "<script>alert('Password tidak sama!');window.location='formuser.php';</script>";
-}elseif($hasil) {
-    echo "<script>alert('Data berhasil disimpan');window.location='datauser.php';</script>";
-    
-} 
+}
+
+elseif($hasil = mysqli_query ($conn, $query)) {
+    $query = "INSERT INTO user (nama_desa, email, password) 
+    VALUE ('$nama_desa', '$email', '$password')";
+    echo "<script>alert('Data berhasil disimpan');window.location='datauser.php';</script>";    
+}
+
 else {
     echo "<script>alert('Data gagal');window.location='formuser.php';</script>";
 }
 
 ?>
-
-
